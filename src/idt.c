08b2462330258db756580408e80408f77b8357a5
penas agustin <apenas@dc.uba.ar>
2017-05-23 22:07:04 -0300

    macro idt hecha

--- src/idt.c
+++ src/idt.c
@@ -33,15 +33,15 @@ idt_descriptor IDT_DESC = {
     }
 */
 
-/*
+
 #define IDT_ENTRY(numero)                                                                                        \
     idt[numero].offset_0_15 = (unsigned short) ((unsigned int)(&_isr ## numero) & (unsigned int) 0xFFFF);        \
-    idt[numero].segsel = (unsigned short) 0x00;                                                                  \
-    idt[numero].attr = (unsigned short) 0x0000;                                                                  \
+    idt[numero].segsel = (unsigned short) 0x60;                                                                  \
+    idt[numero].attr = (unsigned short) 0x0870; //10001110000                                                                 \
     idt[numero].offset_16_31 = (unsigned short) ((unsigned int)(&_isr ## numero) >> 16 & (unsigned int) 0xFFFF);
-*/
+
 
 
 void idt_inicializar() {
-    // Excepciones
+    
 }
