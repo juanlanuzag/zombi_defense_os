15012b546cbc03fb57b0374163c173e8952b1c90
penas agustin <apenas@dc.uba.ar>
2017-06-17 18:44:12 -0300

    hecho el next indice de scheduler

--- src/sched.c
+++ src/sched.c
@@ -13,7 +13,28 @@ info_player playerB = {0,0,0,{0,0,0,0,0,0,0,0},0};
 info_zombie info_zombiesA[CANT_ZOMBIS];
 info_zombie info_zombiesB[CANT_ZOMBIS];
 
+int player = 0;
+
+
+
 unsigned short sched_proximo_indice() {
-  return playerA.gdt_indexes_tasks[0];
+
+	player = player+1 %2;
+	info_player player = player == 0 ? playerA : playerB;
+	unsigned short* gdt_indexes = player.gdt_indexes_tasks;
+	int actual_zombie = player.acct_zombie;
+	actual_zombie=actual_zombie +1 %8;
+	short gdt_index=0;
+	while(actual_zombie!=player.actual_zombie){
+		if(gdt_indexes[actual_zombie]!=0){
+			gdt_index = gdt_indexes[actual_zombie];
+			break;
+		}
+
+		actual_zombie=actual_zombie +1 %8;
+	}
+
+
+  return gdt_index;
 }
 
